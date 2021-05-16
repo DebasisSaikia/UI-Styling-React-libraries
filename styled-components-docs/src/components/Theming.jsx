bd2bc1737eb58479a10f752f987978d7b3badc16
Debasis Saikia <debasishsaikia136@gmail.com>
2021-05-16 19:09:43 +0530

    object styled

--- styled-components-docs/src/components/Theming.jsx
+++ styled-components-docs/src/components/Theming.jsx
@@ -29,6 +29,34 @@ const Card = styled.div`
   background-color: ${(props) => props.theme.sec};
 `;
 
+const Box = styled.div`
+  font-size: 1em;
+  margin: 1em;
+  padding: 0.25em 1em;
+  border-radius: 3px;
+  border: 2px solid ${(props) => props.theme.bdd};
+  color: ${(props) => props.theme.bdd};
+  background-color: ${(props) => props.theme.bg};
+  width: 200px;
+`;
+
+const theme1 = {
+  bdd: "orange",
+  bg: "#white",
+};
+
+const invert = {
+  bg: "black",
+  bdd: "#fff",
+};
+
+// styled objects
+const StObj = styled.div({
+  height: "50px",
+  width: " 50px",
+  background: "red",
+});
+
 function Theming() {
   return (
     <div>
@@ -37,6 +65,12 @@ function Theming() {
       <ThemeProvider theme={theme}>
         <Button>Custom Theming</Button>
         <Card></Card>
+      </ThemeProvider>
+      <ThemeProvider theme={theme1}>
+        <Box>this is box</Box>
+      </ThemeProvider>
+      <ThemeProvider theme={invert}>
+        <Box>this is inverted</Box>
       </ThemeProvider>
     </div>
   );
